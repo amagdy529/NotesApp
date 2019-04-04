45e87870e90d8cf2ee436877c0151c2362b2d445
amagdy529 <amagdy529@gmail.com>
2019-04-04 23:00:09 +0200

    add func

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
-require('./utils.js')
-const fs = require('fs')
+const add = require('./utils.js')
 
-const name = 'Ahmed'
-console.log(name)
 
+
+
+// const fs = require('fs')
+// const sum = add(2,3)
+// const name = 'Ahmed'
+// console.log("sum is :" + sum)
 // fs.writeFileSync('notes.txt','this file was created by node.js , my name is hamada head')
-
-
-
 // fs.appendFileSync('notes.txt','\nthis file was created by node.js , my name is hamada head')
 
--- utils.js
+++ utils.js
@@ -1,1 +1,11 @@
 console.log('console inside utils.js\n')
+
+const utilName = 'hamadaUtil'
+// const secondName = 'second name'
+
+const add = function (a,b) {
+    return a + b
+}
+
+module.exports = add
+// module.exports = secondName
