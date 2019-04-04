c82d20d7bca37f5ac4c5399d2b5859dcc78efabf
amagdy529 <amagdy529@gmail.com>
2019-04-05 00:21:51 +0200

    chalk - BgGreen

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const validator = require('validator')
+const chalk = require('chalk')
 // const getNotes = require('./utils.js')
 const getNotes = require('./notes.js')
 
@@ -7,8 +8,14 @@ const msg = getNotes()
 
 /** logs */
 // console.log(msg)
-console.log(validator.isEmail('test.com'))
+console.log(validator.isEmail('test.com\n'))
 
+// Nest styles of the same type even (color, underline, background)
+console.log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'
+));
 
 
 // const fs = require('fs')
