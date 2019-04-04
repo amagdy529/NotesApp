8db27e7509b0e780e4a5772fcbbfb079942502b4
amagdy529 <amagdy529@gmail.com>
2019-04-04 22:49:19 +0200

    utils

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+
+require('./utils.js')
 const fs = require('fs')
 
+const name = 'Ahmed'
+console.log(name)
 
 // fs.writeFileSync('notes.txt','this file was created by node.js , my name is hamada head')
 
-fs.appendFileSync('notes.txt','\nthis file was created by node.js , my name is hamada head')
+
+
+// fs.appendFileSync('notes.txt','\nthis file was created by node.js , my name is hamada head')
 
---
+++ utils.js
@@ -0,0 +1,1 @@
+console.log('console inside utils.js\n')
