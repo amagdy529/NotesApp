479c72e4ab0077857dfba969150aa949cd2bab7f
amagdy529 <amagdy529@gmail.com>
2019-04-04 22:21:21 +0200

    append file sync

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
 
 
-fs.writeFileSync('notes.txt','this file was created by node.js , my name is hamada head')
+// fs.writeFileSync('notes.txt','this file was created by node.js , my name is hamada head')
+
+fs.appendFileSync('notes.txt','\nthis file was created by node.js , my name is hamada head')
+
