ac5897fd8e9e9503c73a3b1e50d3ba772c4daa63
amagdy529 <amagdy529@gmail.com>
2019-04-05 03:15:35 +0200

    some edits

--- app.js
+++ app.js
@@ -1,37 +1,23 @@
-const validator = require('validator')
-const chalk = require('chalk')
-// const getNotes = require('./utils.js')
-const getNotes = require('./notes.js')
-
-const msg = getNotes()
-
-
-/** logs */
-// console.log(msg)
-console.log(validator.isEmail('test.com\n'))
-
-console.log(process.argv[3])
-
-const command = process.argv[2]
-if (command === 'add') {
-    console.log('adding notes')
-}else if (command === 'remove') {
-    console.log('removing notes')
-}
-
-
-// Nest styles of the same type even (color, underline, background)
-// console.log(chalk.green(
-//     'I am a green line ' +
-//     chalk.blue.underline.bold('with a blue substring') +
-//     ' that becomes green again!'
-// ));
-
-
-// const fs = require('fs')
-// const sum = add(2,3)
-// const name = 'Ahmed'
-// console.log("sum is :" + sum)
-// fs.writeFileSync('notes.txt','this file was created by node.js , my name is hamada head')
-// fs.appendFileSync('notes.txt','\nthis file was created by node.js , my name is hamada head')
-
+const yargs = require('yargs')
+yargs.version('1.1.0')
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+    title: {
+    describe: 'Note title',
+    demandOption: true,
+    type: 'string'
+    },
+    body: {
+    describe: 'Note body',
+    demandOption: true,
+    type: 'string'
+        }
+    },
+    handler: function (argv) {
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
+    }
+})
+console.log(yargs.argv)
