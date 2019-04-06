e95d20560ef14402b9c23fe1ad7a2b1c02d9be0c
amagdy529 <amagdy529@gmail.com>
2019-04-06 17:13:05 +0200

    exception handling

--- notes.js
+++ notes.js
@@ -5,12 +5,36 @@ const getNotes = function () {
 
 const addNote = function (title,body) {
     const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        // console.log(notes)
+        saveNotes(notes)
+        console.log('new note added!')
+    } else {
+        console.log('Note title taken!!')
+    }
+    
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json' , dataJSON)
 }
 
 const loadNotes = function () {
-    const dataBuffer = fs.readFileSync('notes.json')
-    const dataJSON = dataBuffer.toString()
-    return JSON.parse(dataJSON)
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
 }
 
 module.exports = {
