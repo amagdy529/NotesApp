b3ba8c424d8a6209bf79fbffd57aa2fb6792d39a
amagdy529 <amagdy529@gmail.com>
2019-04-06 18:36:52 +0200

    ES6 Refactor

--- app.js
+++ app.js
@@ -21,10 +21,8 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title,argv.body)
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
     }
 })
 
@@ -38,11 +36,36 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
-        console.log('Title: ' + argv.title)
     }
 })
     
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
 yargs.parse()
 // console.log(yargs.argv)
--- notes.js
+++ notes.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function () {
+const getNotes = () => {
     return 'you notes is ...'
 }
 
-const addNote = function (title,body) {
+const addNote  = (title,body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
+    // const duplicateNotes = notes.filter(function (note) {
+    //     return note.title === title
+    // })
     if (duplicateNotes.length === 0) {
         notes.push({
             title: title,
@@ -25,11 +26,12 @@ const addNote = function (title,body) {
 }
 
 
-const removeNote = function (title) {
+const removeNote = (title) =>  {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
+    // const duplicateNotes = notes.filter(function (note) {
+    //     return note.title === title
+    // })
     if (duplicateNotes.length > 0) {
         notes.pop({
             title: title
@@ -43,12 +45,25 @@ const removeNote = function (title) {
     
 }
 
-const saveNotes = function (notes) {
+/** Andrew Remove Note Func */
+// const removeNote = (title) => {
+//     const notes = loadNotes()
+//     const notesToKeep = notes.filter((note) => note.title !== title)
+
+//     if (notes.length > notesToKeep.length) {
+//         console.log(chalk.green.inverse('Note removed!'))
+//         saveNotes(notesToKeep)
+//     } else {
+//         console.log(chalk.red.inverse('No note found!'))
+//     }    
+// }
+
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json' , dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
