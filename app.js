a844879aa34c86ecd02d0fd64130319c913603b4
amagdy529 <amagdy529@gmail.com>
2019-04-06 17:36:47 +0200

    removed unessary lines

--- app.js
+++ app.js
@@ -36,18 +36,11 @@ yargs.command({
             describe: 'Note title',
             demandOption: true,
             type: 'string'
-        },
-        // body: {
-        //     describe: 'Note body',
-        //     demandOption: true,
-        //     type: 'string'
-        // }
+        }
     },
     handler: function (argv) {
         notes.removeNote(argv.title)
-        // notes.addNote(argv.title,argv.body)
         console.log('Title: ' + argv.title)
-        // console.log('Body: ' + argv.body)
     }
 })
     
